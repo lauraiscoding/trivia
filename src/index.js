422eb70d2903c167d3591eb824810af797adcad5
laura <43542652+lauraiscoding@users.noreply.github.com>
2021-01-06 18:30:36 -0500

    End of day

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import './css/App.css';
 import App from './components/App';
 
 // render the App in to the DOM inside the element with id "root"
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('myApp'));
