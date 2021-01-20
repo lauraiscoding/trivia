45f6a79e9d6e8af0ef15f10eeb60c90f1ec0a438
laura <43542652+lauraiscoding@users.noreply.github.com>
2021-01-20 18:22:13 -0500

    Goal 1: done!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,34 @@ import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
 
+function Question(props) {
+  return <div> {props.text}</div>;
+}
+
 function App() {
-  return <div className="app">Trivia!</div>;
+  let currentQuestion = 0;
+
+  return (
+    <div className="app">
+      <Question text={data[0].question.text} />
+      <Question text={data[1].question.text} />
+    </div>
+  );
 }
 
 export default App;
+
+/*
+
+Goal 1: Render a question from sample_data.json on the screen.
+
+- [X] In App.jsx in the App component,
+      add a variable to set the current question number to 0.
+- [X] In App.jsx, create a Question component.
+- [X] Add props to `<Question />` with the text "Question goes here".
+- [X] Render `<Question />` inside of App.
+- [ ] Replace "Question goes here" with the `question.text` field found
+      in data for the first question.
+- [ ] BONUS: Add styling to your app.
+
+*/
